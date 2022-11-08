6612215613ccd55d55d31ef7b9deb2ba92fd9113
Eshwar Seerdat <eshwarseerdat@gmail.com>
2022-11-08 13:03:35 -0500

    add progress feature when previous button clicked

--- script.js
+++ script.js
@@ -18,11 +18,37 @@ const findNextCircle = () => {
 // add active class to object class list and increase progress bar width
 const nextProgress = () => {
   const nextCircle = findNextCircle();
-  if (nextCircle) {
+  if (circlesArr.indexOf(nextCircle) > 0) {
     const pos = circlesArr.indexOf(nextCircle);
     nextCircle.classList.add("active");
     progress.style.width = `${pos * 30}%`;
+    if (pos === circlesArr.length - 1) {
+      nextBtn.toggleAttribute("disabled");
+    }
+  }
+  if (preBtn.hasAttribute("disabled")) {
+    preBtn.toggleAttribute("disabled");
+  }
+};
+
+// search for last element with active class
+// if index of last element greater than 0 remove active class and reduce width of progress bar
+const preProgress = () => {
+  const lastActive = circlesArr.findLast((circle) =>
+    circle.classList.contains("active")
+  );
+  if (circlesArr.indexOf(lastActive) > 0) {
+    const pos = circlesArr.indexOf(lastActive);
+    lastActive.classList.remove("active");
+    progress.style.width = `${pos * 30 - 30}%`;
+    if (pos === 1) {
+      preBtn.toggleAttribute("disabled");
+    }
+  }
+  if (nextBtn.hasAttribute("disabled")) {
+    nextBtn.toggleAttribute("disabled");
   }
 };
 
 nextBtn.addEventListener("click", nextProgress);
+preBtn.addEventListener("click", preProgress);
